4f1164384303800da39c21a8a10d63bc5c982dcd
lazarkirven <lrkirven@gmail.com>
2018-04-03 17:28:03 -0500

    Updates to handle slider events 	modified:   src/pages/about/about.html 	modified:   src/pages/about/about.ts

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,50 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  wholefoods:string = "Savings: $0";
+  wholefoodDiscount:number = 0;
+  uberDiscount:number = 0;
+  salonDiscount:number = 0;
+  restDiscount:number = 0;
+  retailDiscount:number = 0;
+  total:number = 0;
+
+  get formattedMsg():string {
+	return "Savings: $" + this.total;
+  }
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  wholeFoodsChanged(event) {
+    console.log("Whole Foods: " + event.value);
+    this.wholefoodDiscount = event.value;	
+    this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+  }
+
+  uberChanged(event) {
+    console.log("Uber: " + event.value);
+    this.uberDiscount = event.value;	
+    this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+  }
+
+  salonChanged(event) {
+    console.log("Salon: " + event.value);
+    this.salonDiscount = event.value;	
+    this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+  }
+
+  restChanged(event) {
+    console.log("Restaurent: " + event.value);
+    this.restDiscount = event.value;	
+    this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+  }
+
+  retailChanged(event) {
+    console.log("Retail: " + event.value);
+    this.restDiscount = event.value;	
+    this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
   }
 
 }
