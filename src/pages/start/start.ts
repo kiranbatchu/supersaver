a679903e8783d582e900fcb746cdb74f7cc9e907
kiran <kiran@Kirans-MacBook.local>
2018-04-03 09:48:47 -0500

    Your

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController,NavParams} from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 
 
 
@@ -20,3 +20,4 @@ export class StartPage {
   }
 
 }
+   
