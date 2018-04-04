c59551ea7ed9ad6dd7270b9c4e759f66b440cb05
kiran <kiran@kirans-macbook.attlocal.net>
2018-04-03 19:28:48 -0500

    added code

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -13,11 +13,17 @@ export class AboutPage {
   salonDiscount:number = 0;
   restDiscount:number = 0;
   retailDiscount:number = 0;
+  random:number  = 10;
   total:number = 0;
 
   get formattedMsg():string {
 	return "Savings: $" + this.total;
   }
+
+  get formattedMsg2():string {
+   return "Estimated Savings Have Fun in Paris !! " + this.random;
+  }
+
 
   constructor(public navCtrl: NavController) {
 
@@ -27,30 +33,35 @@ export class AboutPage {
     console.log("Whole Foods: " + event.value);
     this.wholefoodDiscount = event.value;	
     this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+    this.random+=1+this.total;
   }
 
   uberChanged(event) {
     console.log("Uber: " + event.value);
     this.uberDiscount = event.value;	
     this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+    this.random+=1+this.total;
   }
 
   salonChanged(event) {
     console.log("Salon: " + event.value);
     this.salonDiscount = event.value;	
     this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+    this.random=1+this.total;
   }
 
   restChanged(event) {
     console.log("Restaurent: " + event.value);
     this.restDiscount = event.value;	
     this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+    this.random=5+this.total;
   }
 
   retailChanged(event) {
     console.log("Retail: " + event.value);
     this.restDiscount = event.value;	
     this.total = this.wholefoodDiscount + this.uberDiscount + this.salonDiscount + this.restDiscount + this.retailDiscount;
+    this.random=1+this.total;
   }
 
 }
